daed63ca9614b4475fae79d6264e165592fa2035
lieman27 <lieman27@bergen.org>
2024-12-15 00:50:23 -0500

    i changed the teleport script
    its randomized based on a generated number, but as the number of rooms increase there will probably be some massive switch statement which isnt efficient, but will work
    
    the blue cube is the one you use, if you walk into it you can be sent to either one of the white cubes

--- Assets/Scripts/TeleportScript.cs
+++ Assets/Scripts/TeleportScript.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic; 
 
+
 public class TeleportScript : MonoBehaviour
 {
     public GameObject Player; 
-    public GameObject TeleportTo; 
+    public GameObject TpLoc1;
+    public GameObject TpLoc2;  
+ 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,7 +23,25 @@ public class TeleportScript : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider o){
-        Player.transform.position = TeleportTo.transform.position; 
+
+        int tpNum = Random.Range(1,3); 
+        string tpStr = tpNum.ToString();
+
+        switch(tpNum)
+        {
+            case 1:
+            Player.transform.position = TpLoc1.transform.position; 
+            Debug.Log("Teleporter 1");
+            
+            break;
+
+            case 2:
+            Debug.Log("Teleporter 2");
+            Player.transform.position = TpLoc2.transform.position; 
+            break;
+
+        }
+        
     }
 
 
