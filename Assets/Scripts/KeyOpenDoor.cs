9124675141eac1c0dfb23516883df75b14682790
NikhilMaalige <nmaalige@gmail.com>
2024-12-15 21:18:52 -0500

    made it destory after 3 seconds

--- Assets/Scripts/KeyOpenDoor.cs
+++ Assets/Scripts/KeyOpenDoor.cs
@@ -23,7 +23,7 @@ public class KeyOpenDoor : MonoBehaviour
 
         if (oo == Key){
             if(Door != null){
-                Destroy(Door);
+                Destroy(Door, 3);
             }
             Debug.Log("success");
         }
