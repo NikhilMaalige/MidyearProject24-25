80813582abefa116f1a37bc3719a53d6f88930d2
lieman27 <lieman27@bergen.org>
2024-12-18 14:42:57 -0500

    new main menu scene that ill have to fix

---
+++ Assets/Scripts/ButtonSwitchScene.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class ButtonSwitchScene : MonoBehaviour
+{
+
+    // public GameObject switchTo; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // public void onClick(){
+    //     SceneManager.LoadScene(switchTo.scene.name);
+
+    // }
+
+}
