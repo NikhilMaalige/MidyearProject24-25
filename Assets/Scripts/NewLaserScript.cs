410a0f93b25643bda0ee5b6bd6cf3f1c895a0fdf
NikhilMaalige <nmaalige@gmail.com>
2024-12-21 15:10:42 -0500

    added a working laser

---
+++ Assets/Scripts/NewLaserScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewLaserScript : MonoBehaviour
+{
+    private LineRenderer lr;
+    [SerializeField]
+    private Transform startPoint;
+
+    void Start() // Fixed capitalization
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.SetPosition(0, startPoint.position); // Set the starting position of the laser
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.right, out hit)) // Use -transform.right for leftward direction
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point); // Set the laser endpoint to the hit point
+            }
+            if (hit.transform.CompareTag("Player")) // Check if the hit object is tagged "Player"
+            {
+                SceneManager.LoadScene("SampleScene"); // Load a new scene if the player is hit
+            }
+        }
+        else
+        {
+            lr.SetPosition(1, transform.position - transform.right * 5000); // Extend the laser far into the distance
+        }
+    }
+}
