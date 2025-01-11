d605e071448bb9992877318b291dea7b3b400e98
NikhilMaalige <nmaalige@gmail.com>
2025-01-11 15:11:45 -0500

    6

--- Assets/Scripts/NewLaserScript.cs
+++ Assets/Scripts/NewLaserScript.cs
@@ -14,27 +14,21 @@ public class NewLaserScript : MonoBehaviour
 
     void Update()
     {
-        // Set the starting position of the laser
         lr.SetPosition(0, startPoint.position);
 
         RaycastHit hit;
 
-        // Adjust raycast and laser direction to point the other way
         if (Physics.Raycast(startPoint.position, startPoint.right, out hit)) 
         {
-            // Set the laser endpoint to the hit point
             lr.SetPosition(1, hit.point);
 
-            // Check if the hit object is tagged "Player"
             if (hit.transform.CompareTag("Player"))
             {
-                // Reset to Level2.1
                 SceneManager.LoadScene("Level2.1");
             }
         }
         else
         {
-            // Extend the laser far in the other direction
             lr.SetPosition(1, startPoint.position + startPoint.right * 5000);
         }
     }
